d34bf223ee7fdcf82b21b4a6f97322d89878bef0
Brendan <brendan.metzger@gmail.com>
2016-01-24 17:13:26 -0600

    no need for all that code

--- bin/ASWM/practice/7/scripts/practice-7.js
+++
@@ -1,5 +0,0 @@
-var assignment_title = 'Add Me';
-
-(function () {
-  console.log('Not Yet');
-}());
--- bin/ASWM/scripts/global.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function (evt) {
-  if (/Helvetica/.test(window.getComputedStyle(document.body).fontFamily)) {
-    document.body.classList.add('correct');
-  }
-});
