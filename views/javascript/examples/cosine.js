efde10eed579091e72225e49e2fd0b123728c2d8
Brendan <brendan.metzger@gmail.com>
2016-03-28 15:07:54 -0500

    working on a homemade cosine function

--- views/javascript/examples/cosine.js
+++ views/javascript/examples/cosine.js
@@ -1,14 +1,21 @@
-var power     = (n, x) => x === 0 ? 1 : n * power(n, x - 1);
-var factorial = n => n === 0 ? 1 : n * factorial(n - 1);
-var deg2rad   = deg => deg / 180 * 3.141592653589793;
+ function deg2rad(deg) {
+  return deg / 180 * 3.141592653589793;
+}
+
+function power(n, x) {
+  return x === 0 ? 1 : n * power(n, x - 1);
+}
+
+function factorial(n) {
+  return n === 0 ? 1 : n * factorial(n - 1);
+}
+
 
 function cosine(angle) {
-  var precision = 15;
-  var n = 0;
   var sum = 0;
-  do {
+  for (var n = 0; n < 15; n++) {
     sum += power(-1, n) * power(angle, 2 * n) / factorial(2 * n);
-  } while (n++ < precision);
+  }
   return sum;
 }
 
