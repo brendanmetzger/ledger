0b3a74b2248c724f3ccb1aaa484c16c6980923ae
Brendan <brendan.metzger@gmail.com>
2016-04-02 14:34:24 -0500

    bread example

---
+++ views/javascript/examples/bread.js
@@ -0,0 +1,78 @@
+var ns = function() {
+
+/* loaf */
+// DECLARE
+function produceBread(ingredients) {
+  return ingredients.join('+') + '=BREAD';
+}
+// APPLY
+var loaf = produceBread(['flour', 'water', 'yeast']);
+/* end loaf */
+
+
+/* compose */
+function produceBread(ingredents) {
+  var dough = mix(ingredients);
+  var bread = bake(dough);
+  return bread;
+}
+/* end compose */
+
+
+
+/* perform */
+// Assume Lou is working, let's call him
+var onDutyBaker = Lou();
+
+// now that we have a Baker, we can do stuff
+var dough = onDutyBaker(mix, ['flour', 'water']);
+var bread = onDutyBaker(bake, dough);
+/* end perform */
+
+/* first class */
+function produceBread(onDutyBaker, ingredients) {
+  var dough = onDutyBaker(mix, ingredients);
+  var bread = onDutyBaker(bake, dough);
+  return bread;
+}
+
+var loaf = produceBread(Sue(), ['flour', 'water']);
+/* end first class */
+
+};
+
+/* baker */
+// Note the arguments of our declaration
+function Baker(performAction, material) {
+  // what we are doing to the first argument
+  return performAction(material);
+}
+/* end baker */
+
+/* people */
+function Sue() {
+  console.log("Sue on duty");
+  return Baker;
+}
+
+function Lou() {
+  console.log("Lou on duty");
+  return Baker;
+}
+
+// what is activity, what can it do?
+var activity = Sue();
+/* end people */
+
+/* purity */
+function mix(ingredients) {
+  return ingredients.join('+');
+}
+function bake(dough) {
+  return dough.toUpperCase().concat(' = BREAD');
+}
+function produceBread(baker, ingredients) {
+  return baker(bake, baker(mix, ingredients));
+}
+
+/* end purity */
