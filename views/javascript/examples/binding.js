a4611e5e3a56a7aa4498a8f73b30728c7bfc530b
Brendan <brendan.metzger@gmail.com>
2016-04-08 10:32:29 -0500

    updating outlines

--- views/javascript/examples/binding.js
+++ views/javascript/examples/binding.js
@@ -17,3 +17,36 @@ function callTwoWays() {
 
 
 /* end new operations */
+
+
+/* currying */
+
+function styler(elem, prop, unit, value) {
+  elem.style[prop] = value + unit;
+}
+
+function Cat(elem) {
+  this.radius = Math.random() * 100;
+  this.top  = styler.bind(null, elem, 'top', 'px');
+  this.left = styler.bind(null, elem, 'left', 'px');
+}
+
+var james = new Cat(document.querySelector("img"));
+
+
+var angle = 0, lastTime = null;
+
+function animate(time) {
+  if (lastTime != null)
+    angle += (time - lastTime) * 0.001;
+  lastTime = time;
+  james.top(Math.sin(angle) * james.radius / 2);
+  james.left(Math.cos(angle) * james.radius);
+  requestAnimationFrame(animate);
+}
+
+document.body.addEventListener('click', animate.bind(null, performance.now()));
+
+
+
+/* end currying */
---
+++ views/javascript/examples/modules.js
@@ -0,0 +1,11 @@
+
+
+(function (exports) {
+    var names = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    exports.name = function(number) {
+      return names[number];
+    };
+    exports.number = function(name) {
+      return names.indexOf(name);
+    }
+})(this.WeekDay = {});
