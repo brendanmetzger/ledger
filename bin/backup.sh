ed5aec2e4fe9057ceef0de804a207688c52b3333
Brendan <brendan.metzger@gmail.com>
2016-09-30 13:59:24 -0500

    adding new stuff

---
+++ bin/backup.sh
@@ -0,0 +1,5 @@
+# Git: add and commit changes
+cd /var/www/pedagogy/data && /usr/bin/git commit -a -m "data push `date`"
+
+# send data to Git server
+cd /var/www/pedagogy/data && /usr/bin/git push origin master
