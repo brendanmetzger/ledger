a6d0f5ea69e1605c3f7e938855641a39016422a1
Brendan <brendan.metzger@gmail.com>
2016-10-18 07:30:40 -0500

    examples and tuning

--- views/javascript/examples/dom.js
+++ views/javascript/examples/dom.js
@@ -1,14 +1,17 @@
 window.addEventListener('load', function() {
 /* click to cut */
-function clickToCut(elem) {
-  elem.innerHTML = '<span>' + elem.innerHTML + '</span>';
+var clickToCut = function (elem) {
+  elem.innerHTML = '<span>' + elem.textContent + '</span>';
   elem.classList.toggle('cutout');
 }
 
-// Assign all h2's click event to react to function above...
-[].forEach.call(document.querySelectorAll('h2'), function(h2) {
-  h2.addEventListener('click', clickToCut.bind(null, h2));
-});
+// find all h2 elements
+var h2s = document.querySelectorAll('h2');
+
+// Loop through h2's and assign click event to apply function above...
+for (var i = 0; i < h2s.length; i++) {
+  h2s[i].addEventListener('click', clickToCut.bind(null, h2s[i]))
+}
 /* end click to cut */
 
 });
