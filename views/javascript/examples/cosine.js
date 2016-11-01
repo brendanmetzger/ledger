fbbc4efb6266e1f9dec330d5749efabcbd3e1b5e
Brendan <brendan.metzger@gmail.com>
2016-10-31 22:21:01 -0500

    updating notes

--- views/javascript/examples/cosine.js
+++ views/javascript/examples/cosine.js
@@ -1,16 +1,21 @@
+/* taylor */
+// unlike us, math folk prefer radians over degrees,
+// so let's make a function that converts them for us.
 function deg2rad(deg) {
   return deg / 180 * 3.141592653589793;
 }
 
+// notice this function calls itself
 function power(n, x) {
   return x === 0 ? 1 : n * power(n, x - 1);
 }
 
+// this one too (it is called recursion)
 function factorial(n) {
   return n === 0 ? 1 : n * factorial(n - 1);
 }
 
-
+// this is a loop and a combitation of functions
 function cosine(angle) {
   var sum = 0;
   for (var n = 0; n < 15; n++) {
@@ -18,6 +23,4 @@ function cosine(angle) {
   }
   return sum;
 }
-
-var rad = deg2rad(45);
-console.log(cosine(rad));
+/* end taylor */
