70497b3ca6a9a68ee945a8462f12f221040fa0f7
Brendan <brendan.metzger@gmail.com>
2016-11-07 14:10:21 -0600

    updating notes

--- views/javascript/examples/loops.js
+++ views/javascript/examples/loops.js
@@ -1,9 +1,34 @@
-/* Loops */
-// A while loop
 
-// A For Loop
+/* for loop */
+var folk = ['Ned', 'Jeb', 'Deb']; // folk[0] is Ned
 
-// Breaking a Loop
+var total = folk.length; // total = 3
+
+for (var i = 0; i < total; i = i + 1) {
+  // i, by convention, stands for index
+  console.log("hello " + folk[i]);
+}
+
+// output is:
+// hello Ned
+// hello Jeb
+// hello Deb
+
+/* end for loop */
 
 
-/* end Loops */
+/* forEach */
+var folk = ['Ned', 'Jeb', 'Deb'];
+
+var sayHelloToPerson = function (name) {
+  console.log("hello " + name);
+};
+// forEach expects a callback function
+folk.forEach(sayHelloToPerson); 
+
+// output is:
+// hello Ned
+// hello Jeb
+// hello Deb
+
+/* end forEach */
