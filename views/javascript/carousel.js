b9fe3719cd998d6fea8325c4ce14cda00f05b77f
Brendan <brendan.metzger@gmail.com>
2017-03-04 18:33:06 -0600

    animation example

---
+++ views/javascript/carousel.js
@@ -0,0 +1,14 @@
+function Carousel(selector = '') {
+  this.position  = 0;
+  this.container = document.querySelector(selector);
+  // this.container.children is our Collection
+}
+
+Carousel.prototype = {
+  draw: function () {
+    
+  },
+  move: function (position) {
+    
+  }
+};
--- views/javascript/examples/objects.js
+++ views/javascript/examples/objects.js
@@ -21,3 +21,26 @@ var bureau = {
 };
 
 /* end basic object */
+
+/* object state */
+// declare an object
+var fido = {};
+// declare a property
+fido.happy = true;
+// declare a method
+fido.scold = function () {
+  fido.happy = false;
+};
+
+// introspect and test  our program
+console.log(fido.happy); // ?
+fido.scold();
+console.log(fido.happy); // ?
+
+// control of flow is an important concept
+if (! fido.happy) {
+  console.log('Tail between the legs.');
+} else {
+  console.log('Look at that tail wag!');
+}
+/* end object state */
